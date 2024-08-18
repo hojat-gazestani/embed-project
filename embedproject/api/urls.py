5ddd28bf00394ddff42dac94999fe3aaf0e4d321
hojat-gazestani <hojatgazestani@gmail.com>
2024-08-18 05:40:56 +0330

    Pytest added

--- embedproject/api/urls.py
+++ embedproject/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', include(('embedproject.blog.urls', 'blog'))),
+    path("blog/", include(("embedproject.blog.urls", "blog"))),
+    path("users/", include(("embedproject.users.urls", "users"))),
+    path("auth/", include(("embedproject.authentication.urls", "auth"))),
     # path('products/', ProductApi.as_view(), name='products'),
 ]
